0151cd427d704f4c4371a1c96ea3c65665f5c129
nguyetph09419 <81679386+nguyetph09419@users.noreply.github.com>
2021-04-08 21:07:28 +0700

    add file

---
+++ javascript.js
@@ -0,0 +1,5 @@
+var indexCurrent = 0;      // Chỉ số hình đầu tiên hiển thị ở slide
+        var loop = true;  // Bật hoặc tắt lặp lại slide
+        var showbutton = true;  // Hiện 2 button điều hướng
+        var duration = 4000;   // Thời gian chờ chuyển hình (tính theo đơn vị milisecond)
+        initSlider();
